99933cb92fd60fe622e06a3a9b318a8718ffd526
Kyryl Andreiev <karilaa@karilaa.dev>
2026-03-24 21:55:03 -0700

    Add stream endpoint and html page

---
+++ nest-app/src/stream-generated/stream-generated.controller.ts
@@ -0,0 +1,17 @@
+import { Controller, Sse } from '@nestjs/common';
+import { map } from 'rxjs/operators';
+import { StreamGeneratedService } from './stream-generated.service';
+
+@Controller()
+export class StreamGeneratedController {
+  constructor(private readonly streamService: StreamGeneratedService) {}
+
+  @Sse('stream')
+  stream() {
+    return this.streamService.getCounterStream().pipe(
+      map((event) => ({
+        data: event,
+      })),
+    );
+  }
+}
